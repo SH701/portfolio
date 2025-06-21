9571887b3497c4156097320877cf26ba39be0cff
SH701 <125341405+SH701@users.noreply.github.com>
2025-06-21 16:50:46 +0900

    서브 프로젝트 추가

--- data/subproject.ts
+++ data/subproject.ts
@@ -8,18 +8,26 @@ interface Props {
 
 export const subProjects: Props[] = [
   {
+  title: "Unwind",
+  description:
+    "Unwind는 로그인, 게시글 작성, 댓글 기능이 포함된 커뮤니티 웹 애플리케이션입니다.",
+  language: "Next.js, Tailwind CSS, Prisma",
+  githubLink: "https://github.com/SH701/twitter_clone",
+  projectLink: "https://unwind-pi.vercel.app/",
+},
+  {
     title: "Portfolio",
     description:
       "seungyn님의 프토폴리오 디자인을 참고해 포트폴리오를 제작했습니다.",
-    language: "NextJs | TailwindCSS | Vercel",
+    language: "NextJs | TailwindCSS",
     githubLink: "https://github.com/SH701/portfolio",
-    projectLink: "",
+    projectLink: "/",
   },
   {
     title: "Next Movie",
     description:
       "Next.js의 라우팅과 데이터 패칭을  영화 API로 학습한 프로젝트입니다.",
-    language: "NextJs | Vercel",
+    language: "NextJs",
     githubLink: "https://github.com/SH701/nextmovie",
     projectLink: "https://nextjsmovie-gamma.vercel.app/",
   },
